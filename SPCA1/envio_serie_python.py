6b9f579f287bbd468f5717bc8ff30c14d2be0863
Ezequiel <support@magneticash.com>
2020-11-25 01:39:16 -0300

    prueba de billeteros y json channel file

--- SPCA1/envio_serie_python.py
+++ SPCA1/envio_serie_python.py
@@ -4,7 +4,13 @@ import serial
 import os
 from time import sleep
 
+channel_board=0
+device_board=0
+
+
 def ImprimirSerie(mbyte):
+    global channel_board
+    global device_board
     serie = serial.Serial('/dev/ttyUSB0', 9600, timeout=0.5, writeTimeout=0)    
     serie.write(bytes(bytearray(mbyte)))#
     print("envio peticion codigo - OK")
@@ -12,18 +18,23 @@ def ImprimirSerie(mbyte):
     while len(q)==0:
         q=serie.read(1024) #aca lee del puerto
         sleep(0.0001)
-    q=q.decode()
+    #q=q.decode()
 
     while True:
-        q=serie.read(1024) #aca lee del puerto
+        qq=serie.read(1024) #aca lee del puerto
         sleep(0.0001)
-        #q=q.decode()
-        print(q)
+        if len(qq)>5:
+            q=qq[2]
+            channel_board=q
+            print('canal '+str(q))
+            q=qq[3]
+            device_board=q
+            print('billetero '+str(q))
 
     print(bytearray(q))
     serie.close()
 
 
-ImprimirSerie([0x53])#aca mandas a la placa lo que queres como parametros
+ImprimirSerie([0x56])#aca mandas a la placa lo que queres como parametros
 
     
